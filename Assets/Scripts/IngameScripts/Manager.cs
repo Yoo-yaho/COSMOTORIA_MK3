0578abc8aa294cb757fb40c9b50f0e1103c5c2c4
BelBelium <96691348+BelBelium@users.noreply.github.com>
2023-01-26 16:37:08 +0900

    IngameTest
    ..

---
+++ Assets/Scripts/IngameScripts/Manager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.SetResolution(1080, 840, false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
